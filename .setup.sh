5efbd73740672c6d4b2dd0de91491c2862a8a05d
Ondrej Zahradnik <ondraz@pm.me>
2024-04-24 14:28:25 +0200

    Update ~/.setup.sh

--- .setup.sh
+++ .setup.sh
@@ -10,22 +10,40 @@ if [ "$1" == "--verbose" ]; then
 	VERBOSE=1
 fi
 
+sudo = 0
+if [[ $EUID -eq 0 ]]; then
+   sudo=1
+fi
+
 update_apt() {
-	sudo apt update
-	sudo apt upgrade -y
+	if [[ $sudo -eq 1 ]]; then
+		sudo apt update
+		sudo apt upgrade -y
+	fi
 }
 
 install_dependencies() {
-	# wget
-	sudo apt install -y wget
+
+	if [[ $sudo -eq 1 ]]; then
+		# wget / docker / xsel
+		sudo apt install -y wget docker xsel
+	fi
+
+	# tpm
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
 install_software() {
-	# mc / tmux / tree / bmap tools
-	sudo apt install -y mc tmux tree bmap-tools
+	if [[ $sudo -eq 1 ]]; then
+		# vim / tmux / tree / g++
+		sudo apt install -y vim-gtk tmux tree g++
+	fi
 }
 
 install_themes() {
+	# vim
+	mkdir -p ~/.vim/colors
+	wget -O ~/.vim/colors/gruvbox.vim https://raw.githubusercontent.com/morhetz/gruvbox/master/colors/gruvbox.vim
 	# gedit
 	wget https://raw.githubusercontent.com/dracula/gedit/master/dracula.xml
 	mkdir -p ~/.local/share/gedit/styles/
@@ -41,7 +59,9 @@ if [ "$VERBOSE" -eq 0 ]; then
 	install_software > /dev/null 2>&1
 	echo "Install themes..."
 	install_themes > /dev/null 2>&1
-	sudo apt autoremove > /dev/null 2>&1
+	if [[ $sudo -eq 1 ]]; then
+		sudo apt autoremove > /dev/null 2>&1
+	fi
 else
 	printf "${yellow}${bold}"
 	echo "Update APT..."
@@ -59,5 +79,7 @@ else
 	echo "Install themes..."
     printf "${reset}"
     install_themes
-	sudo apt autoremove -y
+	if [[ $sudo -eq 1 ]]; then
+		sudo apt autoremove -y
+	fi
 fi
