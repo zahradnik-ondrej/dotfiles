0660a6fa6247cbb370f449fc26ff5a6d7baeea28
Ondrej Zahradnik <ondraz@pm.me>
2024-10-30 13:10:06 +0100

    feat(.setup.sh): update

--- .setup.sh
+++ .setup.sh
@@ -3,39 +3,54 @@
 bold="\e[1m"
 reset="\e[0m"
 yellow="\e[33m"
+red="\e[35m"
+green="\e[32m"
 blue="\e[36m"
 
 # set -x
 
+run() {
+    "$@" >/dev/null 2>&1
+    local exit_code=$?
+
+    if [ $exit_code -eq 0 ]; then
+        printf "${green} ✔${reset}\n"
+    else
+        printf "${red} ✖${reset}\n"
+    fi
+
+    return $exit_code
+}
+
 add_ppas() {
 
 	# kicad
-	printf "${blue}kicad (PPA)${reset}\n"
+	printf "${blue}kicad (PPA)${reset}"
 	sudo add-apt-repository -y ppa:kicad/kicad-8.0-releases
 
 	# love2d
-	printf "${blue}love2d (PPA)${reset}\n"
-	sudo add-apt-repository -y ppa:bartbes/love-stable
-	
+	printf "${blue}love2d (PPA)${reset}"
+	run sudo add-apt-repository -y ppa:bartbes/love-stable
+
 	# obs
-	printf "${blue}obs (PPA)${reset}\n"
-	sudo add-apt-repository -y ppa:obsproject/obs-studio
-	
+	printf "${blue}obs (PPA)${reset}"
+	run sudo add-apt-repository -y ppa:obsproject/obs-studio
+
 	# freecad
-	printf "${blue}freecad (PPA)${reset}\n"
-	sudo add-apt-repository -y ppa:freecad-maintainers/freecad-stable
-	
+	printf "${blue}freecad (PPA)${reset}"
+	run sudo add-apt-repository -y ppa:freecad-maintainers/freecad-stable
+
 	# steam
-	printf "${blue}multiverse (PPA for steam)${reset}\n"
-	sudo add-apt-repository -y multiverse
+	printf "${blue}multiverse (PPA for steam)${reset}"
+	run sudo add-apt-repository -y multiverse
 
 	# peek
-	printf "${blue}peek (PPA)${reset}\n"
-	sudo add-apt-repository -y ppa:peek-developers/stable
+	printf "${blue}peek (PPA)${reset}"
+	run sudo add-apt-repository -y ppa:peek-developers/stable
 
-	# signal
-	printf "${blue}signal (PPA)${reset}\n"
-	echo 'deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main' | sudo tee /etc/apt/sources.list.d/signal-xenial.list
+	# python
+	printf "${blue}python (PPA)${reset}"
+	run sudo add-apt-repository -y ppa:deadsnakes/ppa
 
 }
 
@@ -53,69 +68,77 @@ update_snap() {
 install_dependencies() {
 
 	# flatpak
-	printf "${blue}flatpak${reset}\n"
+	printf "${blue}flatpak${reset}"
+	run sudo apt install -y flatpak
 	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-	sudo apt install -y flatpak
 
 	# docker (for browsh)
-	printf "${blue}docker${reset}\n"
-	sudo snap install docker --yes
+	printf "${blue}docker${reset}"
+	run sudo snap install docker
+
+	# cmake (for youcompleteme for vim)
+	printf "${blue}cmake${reset}"
+	run sudo apt install -y cmake
+
+	# golang (for youcompleteme for vim)
+	printf "${blue}golang${reset}"
+	run sudo apt install -y golang-go
 
 	# youcompleteme (for vim)
 	printf "${blue}youcompleteme${reset}\n"
-	sudo apt install -y cmake golang-go
-	cd "$HOME"
-	[ ! -d "YouCompleteMe" ] && git clone https://github.com/ycm-core/YouCompleteMe.git
+	[ -d "YouCompleteMe" ] && rm -rf "YouCompleteMe"
+	git clone https://github.com/ycm-core/YouCompleteMe.git
 	cd YouCompleteMe
 	git submodule update --init --recursive
-	sudo python3 install.py --all --verbose
+	python3 install.py --all --verbose
 
 	# tpm (for tmux)
-	printf "${blue}tpm${reset}\n"
+	printf "${blue}tpm${reset}"
 	TPM_PATH="$HOME/.tmux/plugins/tpm"
-	[ ! -d "$TPM_PATH" ] && git clone https://github.com/tmux-plugins/tpm "$TPM_PATH"
+	[ -d "$TPM_PATH" ] && rm -rf "$TPM_PATH"
+	run git clone https://github.com/tmux-plugins/tpm "$TPM_PATH"
 
 	# xsel (for tmux)
-	printf "${blue}xsel${reset}\n"
-	sudo apt install -y xsel
+	printf "${blue}xsel${reset}"
+	run sudo apt install -y xsel
 
 	# chromium (for puppeteer)
-	printf "${blue}chromium${reset}\n"
-	sudo apt install -y chromium-browser
+	printf "${blue}chromium${reset}"
+	run sudo apt install -y chromium-browser
 
 	# nodejs
 	printf "${blue}nodejs${reset}\n"
-	wget -qO- https://deb.nodesource.com/setup_20.x | sudo -E bash -
-	sudo apt install -y nodejs
+	wget -qO- https://deb.nodesource.com/setup_20.x | sudo -E bash - && sudo apt install -y nodejs
 
 	# typescript
-	printf "${blue}typescript${reset}\n"
-	npm install typescript @types/node --save-dev
+	printf "${blue}typescript${reset}"
+	run npm install typescript @types/node --save-dev
 
 	# puppeteer / mocha / chai
-	printf "${blue}puppeteer / mocha / chai${reset}\n"
-	npm install puppeteer mocha chai @types/mocha @types/chai --save-dev
+	printf "${blue}puppeteer / mocha / chai${reset}"
+	run npm install puppeteer mocha chai @types/mocha @types/chai --save-dev
 
 	# pip (for yt-dlp)
-	printf "${blue}pip${reset}\n"
-	sudo apt install -y python3-pip
+	printf "${blue}pip${reset}"
+	run sudo apt install -y python3-pip
 
 	# dconf (for dracula for gnome terminal)
-	printf "${blue}dconf${reset}\n"
-	sudo apt install -y dconf-cli
+	printf "${blue}dconf${reset}"
+	run sudo apt install -y dconf-cli
 
 	# expect (for dracula for gnome terminal)
-	printf "${blue}expect${reset}\n"
-	sudo apt install -y expect
+	printf "${blue}expect${reset}"
+	run sudo apt install -y expect
 
 	# appindicator (for proton-vpn)
-	sudo apt install -y libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
+	printf "${blue}appindicator)${reset}"
+	run sudo apt install -y libayatana-appindicator3-1 gir1.2-ayatanaappindicator3-0.1 gnome-shell-extension-appindicator
 
 }
 
 configure_netrc() {
     NETRC_FILE="$HOME/.netrc"
-    
+
     [ -e "$HOME/.private_env_vars" ] && source "$HOME/.private_env_vars"
 
     GITHUB_MACHINE="github.com"
@@ -142,40 +165,40 @@ EOF
 install_software() {
 
 	# vim
-	printf "${blue}vim${reset}\n"
-	sudo apt install -y vim
+	printf "${blue}vim${reset}"
+	run sudo apt install -y vim
 
 	# tmux
-	printf "${blue}tmux${reset}\n"
-	sudo apt install -y tmux
+	printf "${blue}tmux${reset}"
+	run sudo apt install -y tmux
 
 	# tree
-	printf "${blue}tree${reset}\n"
-	sudo apt install -y tree
+	printf "${blue}tree${reset}"
+	run sudo apt install -y tree
 
 	# g++
-	printf "${blue}g++${reset}\n"
-	sudo apt install -y g++
+	printf "${blue}g++${reset}"
+	run sudo apt install -y g++
 
 	# openscad
-	printf "${blue}openscad${reset}\n"
-	sudo apt install -y openscad
+	printf "${blue}openscad${reset}"
+	run sudo apt install -y openscad
 
 	# pycharm
-	printf "${blue}pycharm${reset}\n"
-	sudo snap install pycharm-professional --classic --yes
+	printf "${blue}pycharm${reset}"
+	run sudo snap install pycharm-professional --classic
 
 	# telegram
-	printf "${blue}telegram${reset}\n"
-	sudo snap install telegram-desktop --yes
+	printf "${blue}telegram${reset}"
+	run sudo snap install telegram-desktop
 
 	# slack
-	printf "${blue}slack${reset}\n"
-	sudo snap install slack --yes
+	printf "${blue}slack${reset}"
+	run sudo snap install slack
 
 	# kicad
-	printf "${blue}kicad${reset}\n"
-	sudo apt install -y kicad
+	printf "${blue}kicad${reset}"
+	run sudo apt install -y kicad
 
 	# cura
 	printf "${blue}cura${reset}\n"
@@ -193,16 +216,16 @@ install_software() {
 	fi
 
 	# deluge
-	printf "${blue}deluge${reset}\n"
-	sudo apt install -y deluge
+	printf "${blue}deluge${reset}"
+	run sudo apt install -y deluge
 
 	# vlc
-	printf "${blue}vlc${reset}\n"
-	sudo apt install -y vlc
+	printf "${blue}vlc${reset}"
+	run sudo apt install -y vlc
 
 	# godot
-	printf "${blue}godot${reset}\n"
-	sudo flatpak install -y flathub org.godotengine.Godot
+	printf "${blue}godot${reset}"
+	run sudo flatpak install -y flathub org.godotengine.Godot
 
 	# balenaEtcher
 	printf "${blue}balenaEtcher${reset}\n"
@@ -221,84 +244,96 @@ install_software() {
 
 
 	# yt-dlp
-	printf "${blue}yt-dlp${reset}\n"
-	pip3 install --upgrade yt-dlp
+	printf "${blue}yt-dlp${reset}"
+	run pip3 install --upgrade yt-dlp
 
 	# ffmpeg
-	printf "${blue}ffmpeg${reset}\n"
-	sudo apt install -y ffmpeg
+	printf "${blue}ffmpeg${reset}"
+	run sudo apt install -y ffmpeg
 
 	# nautilus
-	printf "${blue}nautilus${reset}\n"
-	sudo apt install -y nautilus
+	printf "${blue}nautilus${reset}"
+	run sudo apt install -y nautilus
 
 	# surfshark
-	printf "${blue}surfshark${reset}\n"
-	sudo snap install surfshark --beta --yes
+	printf "${blue}surfshark${reset}"
+	run sudo snap install surfshark
 
 	# neofetch
-	printf "${blue}neofetch${reset}\n"
-	sudo apt install -y neofetch
+	printf "${blue}neofetch${reset}"
+	run sudo apt install -y neofetch
 
 	# lua
-	printf "${blue}lua${reset}\n"
-	sudo apt install -y lua5.3
+	printf "${blue}lua${reset}"
+	run sudo apt install -y lua5.3
 
 	# love2d
-	printf "${blue}love2d${reset}\n"
-	sudo apt install -y love
-	
+	printf "${blue}love2d${reset}"
+	run sudo apt install -y love
+
 	# obs
-	printf "${blue}obs${reset}\n"
-	sudo apt install -y obs-studio
-	
+	printf "${blue}obs${reset}"
+	run sudo apt install -y obs-studio
+
 	# freecad
-	printf "${blue}freecad${reset}\n"
-	sudo apt install -y freecad
-	
+	printf "${blue}freecad${reset}"
+	run sudo apt install -y freecad
+
 	# blender
-	printf "${blue}blender${reset}\n"
-	snap install blender --classic --yes
-	
+	printf "${blue}blender${reset}"
+	run sudo snap install blender --classic
+
 	# steam
-	printf "${blue}steam${reset}\n"
-	sudo apt install -y steam
-	
+	printf "${blue}steam${reset}"
+	run sudo apt install -y steam
+
 	# spotify
-	printf "${blue}spotify${reset}\n"
-	sudo snap install spotify
-	
+	printf "${blue}spotify${reset}"
+	run sudo snap install spotify
+
 	# prusaslicer
-	printf "${blue}prusaslicer${reset}\n"
-	sudo flatpak install -y flathub com.prusa3d.PrusaSlicer
-	
+	printf "${blue}prusaslicer${reset}"
+	run sudo flatpak install -y flathub com.prusa3d.PrusaSlicer
+
 	# libreoffice
-	printf "${blue}libreoffice${reset}\n"
-	sudo apt install -y libreoffice
+	printf "${blue}libreoffice${reset}"
+	run sudo apt install -y libreoffice
 
 	# gparted
-	printf "${blue}gparted${reset}\n"
-	sudo apt install -y gparted
+	printf "${blue}gparted${reset}"
+	run sudo apt install -y gparted
 
 	# htop
-	printf "${blue}htop${reset}\n"
-	sudo apt install -y htop
+	printf "${blue}htop${reset}"
+	run sudo apt install -y htop
 
 	# inkscape
-	printf "${blue}inkscape${reset}\n"
-	sudo apt install -y inkscape
+	printf "${blue}inkscape${reset}"
+	run sudo apt install -y inkscape
 
 	# peek
-	printf "${blue}peek${reset}\n"
-	sudo apt install -y peek
+	printf "${blue}peek${reset}"
+	run sudo apt install -y peek
 
 	# flameshot
-	printf "${blue}flameshot${reset}\n"
-	sudo apt install -y flameshot
+	printf "${blue}flameshot${reset}"
+	run sudo apt install -y flameshot
 
 	# signal
-	printf "${blue}signal${reset}\n"
-	sudo apt install -y signal-desktop
+	printf "${blue}signal${reset}"
+	run sudo snap install signal-desktop
+
+	# vivaldi
+	printf "${blue}vivaldi${reset}"
+	run sudo snap install vivaldi
+
+	# dotnet-sdk
+	printf "${blue}dotnet-sdk${reset}"
+	run sudo apt install -y dotnet-sdk-8.0
+
+	# python
+	printf "${blue}python${reset}"
+	run sudo apt install -y python3.12
 
 }
 
@@ -306,7 +341,8 @@ install_themes() {
 
 	# dracula (for gnome terminal)
 	printf "${blue}dracula${reset}\n"
-	[ ! -d "$HOME/gnome-terminal" ] && git clone https://github.com/dracula/gnome-terminal
+	[ -d "gnome-terminal" ] && rm -rf "gnome-terminal"
+	git clone https://github.com/dracula/gnome-terminal
 	cd gnome-terminal
 
 	expect <<EOF
@@ -330,7 +366,15 @@ install_themes() {
 
 EOF
 
+	# dracula (for konsole)
+	[ -d "konsole" ] && rm -rf "konsole"
+	git clone https://github.com/dracula/konsole.git
+	mv "konsole/Dracula.colorscheme" "$HOME/.local/share/konsole"
+	rm -rf "konsole"
+
 }
+
+sudo -v
 
 printf "${yellow}${bold}"
 echo "Add PPAs..."
@@ -338,14 +382,14 @@ printf "${reset}"
 add_ppas
 
 printf "${yellow}${bold}"
-echo "Update APT..."
+printf "Update APT..."
 printf "${reset}"
-update_apt
+run update_apt
 
 printf "${yellow}${bold}"
-echo "Update snap..."
+printf "Update snap..."
 printf "${reset}"
-update_snap
+run update_snap
 
 printf "${yellow}${bold}"
 echo "Install dependencies..."
@@ -353,9 +397,9 @@ printf "${reset}"
 install_dependencies
 
 printf "${yellow}${bold}"
-echo "Configure .netrc..."
+printf "Configure .netrc..."
 printf "${reset}"
-configure_netrc
+run configure_netrc
 
 printf "${yellow}${bold}"
 echo "Install software..."
@@ -363,9 +407,9 @@ printf "${reset}"
 install_software
 
 printf "${yellow}${bold}"
-echo "Install themes..."
+printf "Install themes..."
 printf "${reset}"
-install_themes
+run install_themes
 
-sudo apt autoclean
-sudo apt autoremove -y
+sudo apt autoclean >/dev/null 2>&1
+sudo apt autoremove -y >/dev/null 2>&1
