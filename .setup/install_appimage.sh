e20cd51476333614a16b41d1cb8849780432d650
Ondrej Zahradnik <ondraz@pm.me>
2025-03-01 16:37:02 +0100

    Backup

--- .setup/install_appimage.sh
+++ .setup/install_appimage.sh
@@ -8,16 +8,16 @@ install_appimage() {
   local appimage_file="$4"
 
   local current_version="none"
-  [ -f "$appimage_file" ] && current_version=$("$appimage_file" --appimage-version >/dev/null 2>&1)
+  [ -f "$appimage_path/$appimage_file" ] && current_version=$("$appimage_path/$appimage_file" --appimage-version >/dev/null 2>&1)
 
   if [ "$current_version" != "$expected_version" ]; then
 
     sudo mkdir -p "$appimage_path"
-    run sudo wget -O "$appimage_file" "$download_url"
-    sudo chmod +x "$appimage_file"
+    run sudo wget -O "$appimage_path/$appimage_file" "$download_url"
+    sudo chmod +x "$appimage_path/$appimage_file"
 
   fi
 
-  check test -f "$appimage_file"
+  check test -f "$appimage_path/$appimage_file"
 
 }
