0c4341050ca869c5056109386c41daafb9f864ab
Ondrej Zahradnik <ondraz@pm.me>
2025-04-05 15:05:22 +0200

    deploy

--- .packages/ovim/ovim-install.sh
+++ .packages/ovim/ovim-install.sh
@@ -1,5 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Installing LunarVim core without overwriting config..."
-bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/master/utils/installer/install.sh) --no-config
+set -e
 
+USER_HOME=$(eval echo ~${SUDO_USER:-$USER})  # changed
+OVIM_DIR="$USER_HOME/.local/share/ovim/lvim"  # changed
+OVIM_BIN="$USER_HOME/.local/bin/ovim"  # changed
+OVIM_CONFIG="$USER_HOME/.config/ovim"  # changed
+
+echo "Installing OVim (LunarVim core + custom config) separately..."
+
+mkdir -p "$OVIM_DIR"
+git clone https://github.com/LunarVim/LunarVim.git "$OVIM_DIR" --depth=1
+
+echo "Creating ovim launcher..."
+mkdir -p "$(dirname "$OVIM_BIN")"
+cat > "$OVIM_BIN" <<EOF
+#!/usr/bin/env bash
+NVIM_APPNAME=ovim exec nvim "\$@"
+EOF
+
+chmod +x "$OVIM_BIN"
+
+echo "Copying default OVim config..."
+mkdir -p "$OVIM_CONFIG"
+cp -r /etc/skel/.config/lvim/* "$OVIM_CONFIG/"
+
+echo "Installing LunarVim binary (as ovim)..."
+make -C "$OVIM_DIR" install PREFIX="$USER_HOME/.local"
+
+echo "Done!"
+echo
+echo "You can now run OVim using the 'ovim' command."
+echo "Make sure '$USER_HOME/.local/bin' is in your PATH."
