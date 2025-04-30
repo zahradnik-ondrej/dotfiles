c54b5fe673e2447ac5f0c90436c4b665fdeb5225
Ondrej Zahradnik <ondraz@pm.me>
2025-04-30 11:48:14 +0200

    bak

--- .config/lvim/lua/user/formatters.lua
+++ .config/lvim/lua/user/formatters.lua
@@ -1,23 +1,23 @@
 lvim.format_on_save.enabled = false
 
-local docformatter = {
-  exe = "docformatter",
-  args = {
-    "--in-place",
-    "--wrap-summaries", "120",
-    "--wrap-descriptions", "120",
-  },
-  stdin = true,
-}
+-- local docformatter = {
+--   exe = "docformatter",
+--   args = {
+--     "--in-place",
+--     "--wrap-summaries", "120",
+--     "--wrap-descriptions", "120",
+--   },
+--   stdin = true,
+-- }
 
-local black = {
-  exe = "black",
-  args = { "--line-length", "120" },
-  stdin = false,
-}
+-- local black = {
+--   exe = "black",
+--   args = { "--line-length", "120" },
+--   stdin = false,
+-- }
 
-local formatters = require("lvim.lsp.null-ls.formatters")
-formatters.setup({
-  -- { command = "docformatter", filetypes = { "python" }, args = docformatter.args, stdin = true },
-  { command = "black",        filetypes = { "python" }, args = black.args },
-})
+-- local formatters = require("lvim.lsp.null-ls.formatters")
+-- formatters.setup({
+--   -- { command = "docformatter", filetypes = { "python" }, args = docformatter.args, stdin = true },
+--   { command = "black",        filetypes = { "python" }, args = black.args },
+-- })
